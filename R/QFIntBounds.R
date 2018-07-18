0a374bcd2e06afc9a24ed31d1b2148b7f92c1531
Louis Aslett <louis.aslett@durham.ac.uk>
2018-07-18 16:55:34 +0100

    Recoding on QFIntBounds to include error checking, lower/upper bounds of both lower and upper tails, and correct return names for propagation through the data frame.

--- R/QFIntBounds.R
+++ R/QFIntBounds.R
@@ -1,4 +1,67 @@
-QFIntBounds <- function(obs, M, mu, sigma, k = c(20), resid.op.norm.bd = NULL, log = FALSE) {
+QFIntBounds <- function(obs, M, mu, sigma, k = c(20), resid.op.norm.bd = NULL, lower.tail = TRUE, log = FALSE) {
+
+  # All input must be on the original scale.  obs= y^T M y
+
+  if(class(M) != "dsyMatrix") {
+    stop("Matrix M must be symmetric of class dsyMatrix (from Matrix package).")
+  }
+  if(!all(diag(M) == 0)) {
+    stop("M must be a hollow matrix (zero on diagonal).")
+  }
+  if(!is.vector(obs)) {
+    stop("obs must be a vector.")
+  }
+  # obs <- suppressMessages(as.numeric(crossprod(crossprod(M, y), y)))
+  N <- nrow(M)
+  if(!is.vector(sigma) || length(sigma) != N) {
+    stop("sigma must be a vector matching the number of rows of M.")
+  }
+  if(!is.vector(mu) || length(mu) != N) {
+    stop("mu must be a vector matching the number of rows of M.")
+  }
+
+  # Eigen-decompose and rescale M by N in order to keep magnitude of observations in a more maneagable range
+  mu.tilde <- mu/sigma
+  sigma <- sigma/sqrt(N) # Note: observation vector is rescaled further down
+  M.tilde <- sweep(M * sigma, 2, sigma, "*")
+  e <- eigs(M.tilde, max(k), which = "LM")
+  evec.tilde <- e$vectors[, order(abs(e$values), decreasing=TRUE)]
+  eval.tilde <- e$values[order(abs(e$values), decreasing=TRUE)]
+
+  # Set resid.op.norm.bd to magnitude of smallest truncated eigenvalue if not specified
+  if(is.null(resid.op.norm.bd)) {
+    resid.op.norm.bd <- abs(eval.tilde[length(eval.tilde)])
+  }
+
+  # Compute ncps
+  ncps <- list()
+
+  for(kk in 1:length(k)) {
+    ncps[[kk]] <- c(crossprod(evec.tilde[,1:k[kk]], mu.tilde))^2
+  }
+
+  # nu and E
+  nu <- list()
+  E_R <- list()
+  for(kk in 1:length(k)) {
+    R <- (M.tilde - evec.tilde[,1:k[kk]] %*% (t(evec.tilde[,1:k[kk]]) * eval.tilde[1:k[kk]]))
+    nu[[kk]] <- 8 * sum((R %*% mu.tilde)^2) + 4 * sum(R^2)
+    E_R[[kk]] <- as.numeric(mu.tilde %*% R %*% mu.tilde + sum(diag(R)))
+  }
+
+  # Call complex function
+  res <- data.frame(k = c(), obs = c(), lower = c(), upper = c())
+  for(kk in 1:length(k)) {
+    for(i in 1:length(obs)) {
+      res <- rbind(res, c(k = k[kk],
+                          obs = obs[i],
+                          QFIntBounds2(obs[i]/N, eval.tilde[1:k[kk]], ncps[[kk]], E_R[[kk]], nu[[kk]], resid.op.norm.bd, lower.tail, log)))
+    }
+  }
+  res
+}
+
+QFIntBounds.ryan <- function(obs, M, mu, sigma, k = c(20), resid.op.norm.bd = NULL, log = FALSE) {
 
   require(RSpectra)
 
--- R/QFIntBounds2.R
+++ R/QFIntBounds2.R
@@ -22,11 +22,117 @@
 #' @examples
 #' # Some code here which runs a self-contained example
 #'
-QFIntBounds2 <- function(obs, evals, ncps, E_R, nu, resid.op.norm.bd,log) {
+QFIntBounds2 <- function(obs, evals, ncps, E_R, nu, resid.op.norm.bd, lower.tail = TRUE, log = FALSE) {
+  lower <- 0
+  upper <- 1
 
-  require(CompQuadForm)
+  upper.res <- integrate(QFIntBounds.ineq, lower = 0, upper = Inf,
+                         obs = obs, evals = evals, ncps = ncps, E_R = E_R, nu = nu, resid.op.norm.bd = resid.op.norm.bd, upper.bound = TRUE,
+                         stop.on.error = FALSE)
+  if(upper.res$message != "OK") {
+    warning("Numerical integration failed to achieve required accuracy for upper bound, returning trivial bound.")
+    if(log)
+      upper <- 0
+    else
+      upper <- 1
+  } else {
+    if(log) {
+      if(lower.tail)
+        upper <- log(upper.res$value)
+      else
+        lower <- log(1-upper.res$value)
+    } else {
+      if(lower.tail)
+        upper <- upper.res$value
+      else
+        lower <- 1-upper.res$value
+    }
+  }
 
-  #browser()
+  lower.res <- integrate(QFIntBounds.ineq, lower = 0, upper = Inf,
+                         obs = obs, evals = evals, ncps = ncps, E_R = E_R, nu = nu, resid.op.norm.bd = resid.op.norm.bd, upper.bound = FALSE,
+                         stop.on.error = FALSE)
+  if(lower.res$message != "OK") {
+    warning("Numerical integration failed to achieve required accuracy for lower bound, returning trivial bound.")
+    if(log)
+      lower <- -.Machine$double.xmax
+    else
+      lower <- 0
+  } else {
+    if(log) {
+      if(lower.tail)
+        lower <- log(lower.res$value)
+      else
+        upper <- log(1-lower.res$value)
+    } else {
+      if(lower.tail)
+        lower <- lower.res$value
+      else
+        upper <- 1-lower.res$value
+    }
+  }
+
+  data.frame(lower = lower, upper = upper)
+}
+
+HInt <- function(z, nu, resid.op.norm.bd) {
+  ifelse(z <= nu / (4*resid.op.norm.bd),
+         (z/nu)*exp(-0.5*(z^2)/nu),
+         exp(nu/(32*resid.op.norm.bd^2)-z/(4*resid.op.norm.bd))/(4*resid.op.norm.bd))
+}
+
+SurvivalFuncInt <- function(q, lambda, delta, lim = 20000, acc = 1e-12) {
+
+  result <- davies(q = q,
+                   lambda = lambda,
+                   delta = delta,
+                   lim = lim,
+                   acc = acc)
+
+  if(result$ifault != 0) {
+    if(result$ifault == 1) {
+      out_warn <- "requested accuracy could not be obtained."
+    } else if(result$ifault == 2) {
+      out_warn <- "round-off error possibly significant."
+    } else if(result$ifault == 3) {
+      out_warn <- "invalid parameters."
+    } else if(result$ifault == 4){
+      out_warn <- "unable to locate integration parameters."
+    } else {
+      out_warn <- "davies produced unrcognized fault (not 1, 2, 3, or 4)."
+    }
+    warning(paste("fault in evaluating davies:", out_warn))
+  }
+
+  if(!is.numeric(result$Qq)) {
+    stop("davies returned non-numeric survival function estimate for Qq.")
+  }
+
+  if(result$Qq < 0 || result$Qq > 1) {
+    stop("davies returned survival function estimate for Qq outside of [0,1].")
+  }
+
+  return(result$Qq)
+}
+
+QFIntBounds.ineq <- function(z, obs, evals, ncps, E_R, nu, resid.op.norm.bd, upper.bound = TRUE) {
+  side_indicator <- 2*as.integer(upper.bound)-1
+  HInt(z,nu,resid.op.norm.bd) -
+    HInt(z,nu,resid.op.norm.bd) *
+    SurvivalFuncInt(q = obs-E_R+ side_indicator*z,
+                    lambda = evals,
+                    delta = ncps,
+                    lim = 20000,
+                    acc = 1e-12)
+}
+
+
+
+
+
+################################################################################
+
+QFIntBounds2.ryan <- function(obs, evals, ncps, E_R, nu, resid.op.norm.bd,log) {
 
   upper<-integrate(QFIntBounds.ineq, lower=0, upper=Inf,obs=obs, evals=evals, ncps=ncps, E_R=E_R, nu=nu, resid.op.norm.bd,upper.bound=T)$value
 
@@ -46,14 +152,12 @@ QFIntBounds2 <- function(obs, evals, ncps, E_R, nu, resid.op.norm.bd,log) {
 
 
 
-HInt<- function(z, nu, resid.op.norm.bd) {
+HInt.ryan<- function(z, nu, resid.op.norm.bd) {
   ifelse(z <= nu / (4*resid.op.norm.bd),
          (z/nu)*exp(-0.5*(z^2)/ nu),exp(nu/(32*resid.op.norm.bd^2)-z/(4*resid.op.norm.bd))/(4*resid.op.norm.bd))
 }
 
-QFIntBounds.ineq<- function(z,obs, evals, ncps, E_R, nu, resid.op.norm.bd,upper.bound=T) {
+QFIntBounds.ineq.ryan<- function(z,obs, evals, ncps, E_R, nu, resid.op.norm.bd,upper.bound=T) {
   side_indicator<-2*as.integer(upper.bound)-1
   return(HInt(z,nu,resid.op.norm.bd)-HInt(z,nu,resid.op.norm.bd)*davies(q=obs-E_R+ side_indicator*z,lambda=evals,delta=ncps,lim = 20000, acc = 1e-12)$Qq)
 }
-
-
