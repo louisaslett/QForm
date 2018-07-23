162856c83e34b9f1a1c948d16df8677fd9e03e1f
ryanchrist <ryan.christ7@gmail.com>
2018-07-23 22:40:43 +0100

    trial_of_ap to see if changes to rho give numerical stability to FFT

--- R/bounds_using_FFT.R
+++ R/bounds_using_FFT.R
@@ -2,107 +2,83 @@
 # Evaluating QForm Bounds using FFT
 #####
 
+# Open Questions:
+# 1) Work to get better resolution in the tail....maybe use safer methods?
+# 2) Make sure numerically safe
+# 3) Implement automatic determination of a, b and n based on quantiles desired and bounds on the modulus
+
+# KALIS = kit for acelerated li and stephens
+# Alis = acelerated li and Stephens
+
 #lambda<-c(1e10,-1000:1000,-1e12)
-lambda<-c(-5:5,-10,20,30)
+lambda<-c(200,-300,100:110)
 ncps<-1:length(lambda)
 
+# If you don't make a and b large enough, you get nonsense
 
 
-a<--10000
-b<-10000
-n<-200000
-
+a<- -1000000
+b<- 1000000
+n<-2^10
+# There appears to be a limit...the relative error with respect to davies doesn't improve with increasing n.
 log.phi<-function(t,lambda,ncps){
   sum(complex(imaginary=ncps*t*lambda)/complex(real=1, imaginary=-2*t*lambda) - 0.5*log(complex(real=1,imaginary=-2*t*lambda)))
 }
 
+
 rho<-1:n
-for(k in 1:n){
-  rho[k]<-exp(log.phi( (pi*n/(b-a))*(2*((k-1)/n)-1), lambda, ncps) - complex(imaginary = pi*a*(n/(b-a))*(2*((k-1)/n)-1)) )
-  print(k)
-}
+for(k in 1:n)  rho[k]<-exp(log.phi( (pi*n/(b-a))*(2*((k-1)/n)-1), lambda, ncps) - complex(imaginary = pi*a*(n/(b-a))*(2*((k-1)/n)-1)) )
+
+xx<-seq(a,b-(b-a)/n,length.out = n)
+
+plot(xx,-log10(rho),type="l")
 
 ans<-Re(fft(rho)*(-1)^(0:(n-1))/(b-a))
-xx<-seq(a,b-(b-a)/n,length.out = n)
-plot(xx, ans,type="l",xlab="Q",ylab="density")
 
+plot(xx,-log10(ans),type="l")
+lines(xx,-log10(ans2),col="red")
+
+
+dF<-ifelse(ans<10^(-16),10^(-16),ans)
+
+plot(xx, dF,type="l",xlab="Q",ylab="density")
+plot(xx, -log10(dF),type="l",xlab="Q",ylab="density")
+
+# Here we verify that we're getting the same answer as CompQuadForm
+
+require(CompQuadForm)
 true<-xx
 for( i in 1:length(xx)){
 true[i]<-1-davies(q =xx[i],lambda = lambda,delta = ncps)$Qq
 }
-
 plot(true,type="l",col="blue")
 lines(cumsum(ans)*((b-a)/n),type="l",col="red")
-plot((cumsum(ans)*((b-a)/n)-true),type="l")
+plot((cumsum(ans)*((b-a)/n)-true)/true,type="l")
+plot(true-(cumsum(ans)*((b-a)/n))/(cumsum(ans)*((b-a)/n)),type="l")
 
 
+# Now lets integrate this with respect to our bound
 
-require(CompQuadForm)
-
-davies(q =  ,lambda = lambda,delta = ncps)$Qq
-
-require(parallel)
-
-sum.phi<-function(k,n,l,lambda,ncps){
-  #cl<-makeCluster(detectCores() - 1, type="FORK")
-  #clusterExport(cl,c("t","lambda","ncps","log.phi"))
-  #ans<-parSapply(cl, X = -l:l, function(x){exp(log.phi((n/(b-a))*(2*pi*((k-1)/n)+x),lambda,ncps)-complex(imaginary = a*(n/(b-a))*(2*pi*((k-1)/n)+x)))})
-  ans<-sapply(X = -l:l, function(x){exp(log.phi((n/(b-a))*(2*pi*((k-1)/n)+x),lambda,ncps)-complex(imaginary = a*(n/(b-a))*(2*pi*((k-1)/n)+x)))})
-  #stopCluster(cl)
-  sum(ans)
+# Boole Quadrature Method
+boole<-function(x,a,b){
+  n<-length(x)
+  if((n-1)%%4!=0) stop("x must be of length 4N+1 for some natural number N")
+  w<-c(14,32,12,32)
+  ((b-a)/(n-1))*(2/45)*(sum(w*x[-n])+7*x[n]-7*x[1])
 }
 
-y<-1:n
-for(k in 1:n){
-y[k]<-sum.phi(k,n,1000,lambda,ncps)
-print(k)
-}
 
-x<-fft(y,inverse=FALSE)/(b-a)
-plot(Re(x))
-#
+# User provides
+N<-8
+a<-0
+b<-pi
 
+# Calculate end point
+b.prime<-b+3*(b-a)/((2^N)-4)
 
-#require(PreciseSums)
+# Evaluate function on grid with 2^N points and throw last 3 away
+xx<-seq(a,b.prime,length.out = 2^N)
+yy<-dcauchy(xx,scale = 10)[-c(2^N-0:2)]
 
-# logphi<-function(t){
-#   v<-complex(imaginary=ncps*t*lambda)/complex(real=1, imaginary=-2*t*lambda) - 0.5*log(complex(real=1,imaginary=-2*t*lambda))
-#   complex(real=fsum(Re(v)),imaginary=fsum(Im(v)))
-# }
-#
-#
-# sum.phi<-function(t,l){
-#   v<--l:l
-#   for(i in -l:l){
-#     v[i] <- logphi(2*pi*t+i)
-#   }
-#   mrv<-max(Re(v))
-#   miv<-max(Im(v))
-#   w<-exp(v-complex(real=mrv,imaginary=miv))
-#   exp(complex(real=mrv,imaginary=miv)+log(complex(real=fsum(Re(w)),fsum(Im(w)))))
-# }
-
-sum.phi(2,10000)
-
-
-
-
-xx<-seq(0,10,by=0.001)
-yy<-xx
-for(i in 1:length(xx)){
-  yy[i]<-phi(xx[i])
-}
-
-zz<-fft(yy,inverse = TRUE)/length(yy)
-plot(xx,zz,type="l")
-
-zz2<-fft(zz,inverse=FALSE)
-plot(xx,zz2,type="l")
-
-
-plot(xx,Re(yy),type="l")
-#
-
-
-complex(imaginary=ncps*t*lambda)
-complex(real=c(1,2,3),imaginary=1)
+# Check the error
+print(boole(yy,0,pi)-(pcauchy(b,scale=10)-pcauchy(a,scale=10)),digits=20)
---
+++ R/bounds_using_FFT_ap.R
@@ -0,0 +1,52 @@
+
+#lambda<-c(1e10,-1000:1000,-1e12)
+lambda<-mpfr(c(200,-300,100:110),acc)
+ncps<-mpfr(1:length(lambda),acc)
+
+
+a<- mpfr(-1000000,acc)
+b<- mpfr(1000000,acc)
+n.forR<-2^16
+n<-mpfr(n.forR,acc)
+
+
+c.div <- function(x, y) {
+  d<-rowSums(y^2)
+  matrix(c(
+    (x[,1]*y[,1] + x[,2]*y[,2])/d,
+    (x[,2]*y[,1] - x[,1]*y[,2])/d
+  ), nrow = nrow(x), ncol = 2)
+}
+
+log.phi.ap<-function(t,lambda,ncps){
+  x<-cbind(mpfr(0,acc),ncps*t*lambda)
+  y<-cbind(mpfr(1,acc),-2*t*lambda)
+  matrix(colSums(c.div(x,y) - 0.5*c.log(y)),ncol=2)
+}
+
+rho.ap<-mpfr(matrix(0,nrow=n.forR,ncol=2),acc)
+require(foreach)
+require(doMC)
+registerDoMC(cores = 4)
+
+rho.ap<-foreach(k = 1:n.forR,.combine=rbind) %dopar% exp(log.phi.ap((Const("pi",acc)*n/(b-a))*(2*((k-1)/n)-1), lambda, ncps) - matrix(c(mpfr(0,acc),Const("pi",acc)*a*(n/(b-a))*(2*((k-1)/n)-1)),ncol=2))
+
+rho<-complex(real=as.double(rho.ap[,1]),imaginary=as.double(rho.ap[,2]))
+
+complex(imaginary = pi*a*(n/(b-a))*(2*((k-1)/n)-1))
+
+xx<-seq(a,b-(b-a)/n.forR,length.out = n.forR)
+
+plot(xx,-log10(rho.ap[,1]),type="l")
+
+ans<-Re(fft(rho)*(-1)^(0:(n-1))/(b-a))
+
+ans2<-fft.ap(rho)[,1]*(-1)^(0:(n-1))/(b-a)
+
+plot(xx,-log10(ans),type="l")
+lines(xx,-log10(ans2),col="red")
+
+
+
+ans2<-fft.ap(rho)[,1]*(-1)^(0:(n-1))/(b-a)
+
